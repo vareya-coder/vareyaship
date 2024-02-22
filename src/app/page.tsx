2cc01a57d636346d29f68215d9939cec17ba4936
unknown <syedhasnain769@gmail.com>
2024-02-23 02:55:13 +0500

    new fix 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Hero from './screens/Hero'
 import { getAllShipmentDetails, getOrderDetails } from '@/lib/db/dboperations'
 import { ShipmentDetailsType } from '@/lib/db/schema'
+
+export const dynamic = 'force-dynamic'
 export default async  function Home() {
  
   return (
