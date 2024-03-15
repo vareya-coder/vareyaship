71b16fd5ea9106145c08c21050aedf3b3dfed227
ai-work <ai-work>
2024-03-15 09:05:45 +0500

    Tracking link changed for all of PostNL methods

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,6 @@
 import {neon , neonConfig} from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 
-import { logger } from '@/utils/logger';
-
-logger.info(process.env.DATABASE_URL!);
 const sql = neon(process.env.DATABASE_URL!) 
 
 export const db =drizzle(sql)
